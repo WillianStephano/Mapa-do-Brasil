eaca9cdb9fe1f4aacc8846deaed34f3c6f657c77
Willian Stephano <will.stephano@gmail.com>
2022-05-27 22:50:33 -0300

    Realização de conexão com API e criação de interatividade com mapa

--- Js/main.js
+++ Js/main.js
@@ -5,7 +5,33 @@ for (let i = 0; i < colecaoEstados.length; i++) {
    const estado = colecaoEstados[i];
    
    estado.addEventListener('click' ,function () {
-      console.log(estado);
+      var uf = estado.id
+      var ufFormatado = uf.slice(3)
+      
+      API(ufFormatado)   
    })
    
 }
+
+
+const API = (uf) =>{
+   const url = `https://brasilapi.com.br/api/ibge/uf/v1/${uf}`
+   fetch(url).then(responde =>responde.json()).then(console.log);
+   
+   console.log(uf.nome);
+}
+
+
+
+
+/* 
+Codigo feito desta forma temporaria pois por algum motivo desconhecido
+nao consegui trabalhar com funções separadas e chamando elas conforme a necessidade.
+*/
+
+
+
+
+
+
+
